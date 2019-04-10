727bd43574382241be2d7e67488c9c311d964a70
Esra Oktav Dinler <oktavesra@gmail.com>
2019-04-10 16:01:24 +0200

    :sparkles: Added HttpCliendModule & removed stuff that is no longer needed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,32 +2,24 @@ import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { NavbarComponent } from './navbar/navbar.component';
 import { StartpageComponent } from './startpage/startpage.component';
-import { RecipeComponent } from './recipe/recipe.component';
-// import { RecipeSuggComponent } from './recipe-sugg/recipe-sugg.component';
-// import { Navbar2Component } from './navbar2/navbar2.component';
-// import { RecipeListComponent } from './recipe-list/recipe-list.component';
+// import { RecipeComponent } from './recipe/recipe.component';
 import { RecipeDetailComponent } from './recipe-detail/recipe-detail.component';
 import { SearchResultsComponent } from './search-results/search-results.component';
 import { SearchOptionsBarComponent } from './search-options-bar/search-options-bar.component';
+import { HttpClientModule } from '@angular/common/http';
 
-//import stuff like navigation bar, frontpage, recipe page etc.
-// + Add to declorations and imports
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
     StartpageComponent,
-    RecipeComponent,
-    // RecipeSuggComponent,
-    // Navbar2Component,
-    // RecipeListComponent,
+    // RecipeComponent,
     RecipeDetailComponent,
     SearchResultsComponent,
     SearchOptionsBarComponent
@@ -37,6 +29,7 @@ import { SearchOptionsBarComponent } from './search-options-bar/search-options-b
     AppRoutingModule,
     FontAwesomeModule,
     FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
