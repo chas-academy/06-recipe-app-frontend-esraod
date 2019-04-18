9269b51aab81d126dd0f404c095abd84498359ba
Esra Oktav Dinler <oktavesra@gmail.com>
2019-04-18 18:46:23 +0200

    :zap:

--- src/app/search-options-bar/search-options-bar.component.ts
+++ src/app/search-options-bar/search-options-bar.component.ts
@@ -1,6 +1,7 @@
 import { RecipeService } from './../services/recipe.service';
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { faHeart, faUser, faSearch } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-search-options-bar',
@@ -9,18 +10,27 @@ import { Router } from '@angular/router';
 })
 export class SearchOptionsBarComponent implements OnInit {
 
-  search: any;
+  // search: any;
+  
+  user = faUser;
+  heart = faHeart;
+  search = faSearch;
 
   constructor(
     private recipeService: RecipeService,
     private router: Router,
   ) { }
+  
+  
 
   ngOnInit() {
   }
 
   sendToUrl(value){
-    this.router.navigateByUrl(`/recipes/${value.form.value.searchValue}`)
+    console.log(value.form.value)
+    
+    this.router.navigateByUrl(`/recipes/${value.form.value.searchTerm}`)
   }
+  
 
 }
