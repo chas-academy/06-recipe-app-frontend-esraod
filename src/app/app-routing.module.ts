8ed3bd9cd9c7081c7b01ed22470b59a30adb5f91
Esra Oktav Dinler <oktavesra@gmail.com>
2019-04-18 12:46:29 +0200

    Moved search functionality and the results into the same component, and made possible for the user to search with allergen, diet and course preferences

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { RecipeDetailComponent } from './recipe-detail/recipe-detail.component';
 import { SearchResultsComponent } from './search-results/search-results.component';
-import { SearchOptionsBarComponent } from './search-options-bar/search-options-bar.component';
 
 
 // add import for routing for navigaton bar,startpage recipe page etc.
@@ -14,13 +13,21 @@ const routes: Routes = [
     path: 'recipe/:id',
     component: RecipeDetailComponent,
   },
+  
   {
-    path: 'recipes/:value',
+    path: 'recipes',
     component: SearchResultsComponent,
   },
+
+  // {
+  //   path: 'search',
+  //   component: SearchOptionsBarComponent,
+  // },
+
   {
-    path: 'search',
-    component: SearchOptionsBarComponent,
+    path: '',
+    redirectTo: '/recipes',
+    pathMatch: 'full',
   },
 
 ];
--- src/app/search-results/search-results.component.ts
+++ src/app/search-results/search-results.component.ts
@@ -1,6 +1,7 @@
 import { ActivatedRoute } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
 import { RecipeService } from '../services/recipe.service';
+import { faSearch } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-search-results',
@@ -11,22 +12,73 @@ export class SearchResultsComponent implements OnInit {
   
   recipes: any
 
+  search = faSearch;
+
   constructor(
     private route: ActivatedRoute,
     private recipeService: RecipeService,
   ) { }
 
   ngOnInit() {
-    this.route.params.subscribe(routeParams =>{
-      let val = this.route.snapshot.paramMap.get('value')
-      console.log(val);
+    // this.route.params.subscribe(routeParams =>{
+    //   let val = this.route.snapshot.paramMap.get('value')
+      
+    //   console.log(val);
 
-      this.recipeService.getRecipes(val).subscribe(data => {
-        this.recipes = data
-        console.log(this.recipes)
-        this.recipes = this.recipes.matches
-      })
+    //   this.recipeService.getRecipes(val).subscribe(data => {
+    //     this.recipes = data
+    //     console.log(this.recipes)
+    //     this.recipes = this.recipes.matches
+    //   })
+    // })
+  }
+
+  sendToUrl(value) {
+
+    console.log(value.form.value);
+
+    let finalOptions = ""
+
+    let searchterm = value.form.value.searchTerm
+
+    let allergy = ""
+    if (value.form.value.dairy == true){
+      allergy += "&allowedAllergy[]=396^Dairy-Free"
+    }
+    if (value.form.value.gluten == true){
+      allergy += "&allowedAllergy[]=393^Gluten-Free"
+    }
+
+    let diet = ""
+    if (value.form.value.vegan == true){
+      diet += "&allowedDiet[]=386^Vegan"
+    }
+    if (value.form.value.vegetarian == true){
+      allergy += "&allowedDiet[]=387^Lacto-ovo+Vegetarian"
+    }
+
+    let course = ""
+    if (value.form.value.appetizers == true){
+      course += "&allowedCourse[]=course^course-Appetizers"
+    }
+    if (value.form.value.main == true){
+      allergy += "&allowedCourse[]=course^course-Main-Dishes"
+    }
+    if (value.form.value.desserts == true){
+      allergy += "&allowedCourse[]=course^course-Desserts"
+    }
+
+    finalOptions += searchterm
+    finalOptions += allergy
+    finalOptions += diet
+    finalOptions += course
+
+    this.recipeService.getRecipes(finalOptions).subscribe(data => {
+      this.recipes = data
+      console.log(this.recipes)
+      this.recipes = this.recipes.matches
     })
+
   }
 
   
