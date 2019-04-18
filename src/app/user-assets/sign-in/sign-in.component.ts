ca0ee6081b542146e4707b5afd8f6dd3f5b6d213
Esra Oktav Dinler <oktavesra@gmail.com>
2019-04-18 18:47:29 +0200

    signup & signin, changed input type to passwords, removed name & confirm password

--- src/app/user-assets/sign-in/sign-in.component.ts
+++ src/app/user-assets/sign-in/sign-in.component.ts
@@ -1,4 +1,7 @@
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { AuthenticationService } from './../../services/authentication.service';
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-sign-in',
@@ -7,9 +10,43 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignInComponent implements OnInit {
 
-  constructor() { }
+ 
+  constructor(
+    private auth: AuthenticationService,
+    private router: Router
+  ) { }
 
   ngOnInit() {
   }
+
+
+  loginForm = new FormGroup({
+    email: new FormControl('',[
+      Validators.required,
+      Validators.email,
+    ]),
+    password: new FormControl('',[
+      Validators.required,
+      Validators.minLength(6),
+    ]),
+  })
+
+  loginAccount() {
+    const credentials = {
+      email: this.loginForm.value.email,
+      password: this.loginForm.value.password,
+    }
+
+    this.auth.login(credentials).subscribe(
+      () => {
+        this.router.navigateByUrl('/saved')
+      },
+      err => {
+        console.error(err)
+      }
+    )
+
+  }
+
 
 }
--- src/app/user-assets/sign-up/sign-up.component.ts
+++ src/app/user-assets/sign-up/sign-up.component.ts
@@ -1,4 +1,7 @@
+import { Router } from '@angular/router';
+import { AuthenticationService } from './../../services/authentication.service';
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-sign-up',
@@ -7,9 +10,48 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignUpComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private auth: AuthenticationService,
+    private router: Router 
+  ) { }
 
   ngOnInit() {
   }
+
+  registerForm = new FormGroup({
+    email: new FormControl('',[
+      Validators.required,
+      Validators.email,
+    ]),
+    password: new FormControl('',[
+      Validators.required,
+      Validators.minLength(6),
+    ]),
+  })
+
+  createAccount() {
+    const credentials = {
+      email: this.registerForm.value.email,
+      password: this.registerForm.value.password,
+    }
+
+    this.auth.register(credentials).subscribe(
+      () => {
+        this.auth.login(credentials).subscribe(
+          () => {
+            this.router.navigateByUrl('/saved')
+          },
+          err => {
+            console.error(err)
+          }
+        )
+      },
+      err => {
+        console.error(err)
+      }
+    )
+  }
+
+
 
 }
